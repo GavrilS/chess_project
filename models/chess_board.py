ca8e3bd78d3a79104f332c987ea0c6f683b56a7e
GavrilS <gavr1lll@abv.bg>
2023-12-15 21:08:09 +0200

    Added placeholders for base pieces class and board class

---
+++ models/chess_board.py
@@ -0,0 +1,4 @@
+"""This is the class that will implement the chess board."""
+
+class ChessBoard:
+    pass
---
+++ models/chess_pieces/basic.py
@@ -0,0 +1,9 @@
+"""This class signifies a basic chess piece and lists the necessary fields and methods a piece needs to 
+implement.
+"""
+
+
+class ChessPiece:
+
+    def __init__(self):
+        pass
