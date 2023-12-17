e747873a99fc2583a934f90934d786ea7b563d6a
GavrilS <gavr1lll@abv.bg>
2023-12-17 13:35:54 +0200

    Adding functionality to add figures to the board : WIP

--- models/chess_board.py
+++ models/chess_board.py
@@ -1,6 +1,16 @@
 """This is the class that will implement the chess board."""
+from models.chess_pieces.basic import ChessPiece
 
-EMPTY_FIELD_VALUE = 0
+WHITE_FIELD_VALUE = 0
+BLACK_FIELD_VALUE = 1
+POS_ONE_ROWS = (0, 1)
+POS_TWO_ROWS = (6, 7)
+ROOK_POS = (0, 7)
+KNIGHT_POS = (1, 6)
+BISHOP_POS = (2, 5)
+ROYAL_POS = (3, 4)
+PAWN_POS = (0, 1, 2, 3, 4, 5, 6, 7)
+
 
 class ChessBoard:
     
@@ -17,17 +27,54 @@ class ChessBoard:
             print()
 
 
-    def _populate_board(self):
-        pass
+    def _populate_board(self, pos_one_pieces, pos_two_pieces):
+        for piece in pos_one_pieces:
+            self._verify_piece(piece)
+            pawn_row = POS_ONE_ROWS[1]
+            non_pawn_row = POS_ONE_ROWS[0]
+            if piece.rank == 'pawn':
+                col = self._check_pos(row=pawn_row, PAWN_POS)
+                self.set_piece(row=pawn_row, col, piece)
+            elif piece.ran = 'rook':
+                col = self._check_pos(row=non_pawn_row, ROOK_POS)
+                self.set_piece(row=non_pawn_row, col, piece)
+
+
+    def set_piece(self, row, col, piece):
+        self._board[row][col] = piece
+
+    
+    def _check_pos(self, row, columns):
+        for i in columns:
+            if not isinstance(self._board[row][i], ChessPiece):
+                return i
+        print('No empty position found among the supplied options for this piece... Exiting!')
+        exit()
+
+
+    def _verify_piece(self, piece):
+        if not isinstance(piece, ChessPiece):
+                print('A piece was provided that does not belong to the board... Exiting!')
+                exit()
 
 
     def _build_board(self):
         board = []
+        current_field_val = WHITE_FIELD_VALUE
         for i in range(8):
             row = []
             board.append(row)
             for j in range(8):
-                board[i].append(EMPTY_FIELD_VALUE)
+                board[i].append(current_field_val)
+                if current_field_val == 0:
+                    current_field_val = 1
+                else:
+                    current_field_val = 0
+
+            if current_field_val == 0:
+                current_field_val = 1
+            else:
+                current_field_val = 0
 
         self._board = board
 
