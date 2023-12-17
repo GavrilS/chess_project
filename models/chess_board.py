d9bcea723cf22420b387413443d7b2c5ce7fdfd2
GavrilS <gavr1lll@abv.bg>
2023-12-17 13:00:03 +0200

    Added build method for the board

--- models/chess_board.py
+++ models/chess_board.py
@@ -1,4 +1,40 @@
 """This is the class that will implement the chess board."""
 
+EMPTY_FIELD_VALUE = 0
+
 class ChessBoard:
-    pass
+    
+
+    def __init__(self):
+        self._build_board()
+
+
+    def status(self):
+        # print('*******Board*******')
+        for i in range(8):
+            for j in range(8):
+                print(self._board[i][j], end=' ')
+            print()
+
+
+    def _populate_board(self):
+        pass
+
+
+    def _build_board(self):
+        board = []
+        for i in range(8):
+            row = []
+            board.append(row)
+            for j in range(8):
+                board[i].append(EMPTY_FIELD_VALUE)
+
+        self._board = board
+
+
+
+if __name__=='__main__':
+    print('Testing chess board class creation.')
+    b = ChessBoard()
+    b.status()
+    print('Test finished.')
