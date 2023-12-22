9027996aa8e31d630a02a70293439e893bb70d36
GavrilS <gavr1lll@abv.bg>
2023-12-22 21:29:03 +0200

    Added config files for building local models to more easily import models and added the build of the package to the setup.sh script

--- setup/python_env.sh
+++ setup/python_env.sh
@@ -12,5 +12,5 @@ fi
 
 echo "Python version found and script is continuing"
 
-python3 -m pip install virtualenv && python3 -m venv env && source env/bin/activate \
-    && pip install -r requirements.txt
+python3 -m pip install virtualenv && python3 -m venv ../env && source ../env/bin/activate \
+    && pip install -r requirements.txt && cd ../models_package/ && python3 -m pip install -e .
