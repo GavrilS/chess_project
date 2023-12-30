f595b567d066f96480ad602f23d13e38313ebc8d
GavrilS <gavr1lll@abv.bg>
2023-12-30 14:27:37 +0200

    Adding move functionality to bishop piece -> WIP

--- models_package/models/chess_pieces/bishop.py
+++ models_package/models/chess_pieces/bishop.py
@@ -1,5 +1,10 @@
 from chess_pieces.basic import ChessPiece
 
+END_ROW = 7
+START_ROW = 0
+START_COL = 0
+END_COL = 7
+
 
 class Bishop(ChessPiece):
 
@@ -7,12 +12,47 @@ class Bishop(ChessPiece):
         super().__init__(rank='bishop', color=color)
 
 
-    def move(self, board, position):
+    def move(self, board, position, next_position):
         pass
 
 
-    def check_available_moves(self, board):
-        pass
+    def check_available_moves(self, board, position):
+        possible_moves = []
+        current_coordinates = position.split(':')
+        int(row) = current_coordinates[0]
+        int(col) = current_coordinates[1]
+        current_row = row
+        current_col = col
+        flag = True
+        # Move up left diagonal
+        while flag:
+            flag = False
+            if current_row-1 >= START_ROW and current_row-1 <= END_ROW and current_col-1 >= START_COL and current_col-1 <= END_COL:
+                if not board[row-1][col-1].piece or board[row-1][col-1].piece.color != self.color:
+                    possible_moves.append(str(current_row-1) + ':' + str(current_col-1))
+                    current_row -= 1
+                    current_col -= 1
+                    flag = True
+        
+        current_row = row
+        current_col = col
+        flag = True
+        # Move up right diagonal
+        while flag:
+            flag = False
+            if current_row-1 >= START_ROW and current_row-1 <= END_ROW and current_col+1 >= START_COL and current_col+1 <= END_COL:
+                if not board[row-1][col+1].piece or board[row-1][col+1].piece.color != self.color:
+                    possible_moves.append(str(current_row-1) + ':' + str(current_col+1))
+                    current_row -= 1
+                    current_col += 1
+                    flag = True
+
+        current_row = row
+        current_col = col
+        flag = True
+        # Move down left diagonal
+        while flag:
+            flag = False
 
 
     def __str__(self):
