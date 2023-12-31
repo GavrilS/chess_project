cb0c19445604cb9e403627c0c82ce16904cfe81d
GavrilS <gavr1lll@abv.bg>
2023-12-31 13:02:31 +0200

    Added move functionality for the king piece

--- models_package/models/chess_pieces/king.py
+++ models_package/models/chess_pieces/king.py
@@ -7,12 +7,42 @@ class King(ChessPiece):
         super().__init__(rank='king', color=color)
 
 
-    def move(self, board, position):
-        pass
+    def check_available_moves(self, board, position):
+        possible_moves = []
+        current_coordinates = position.split(':')
+        int(row) = current_coordinates[0]
+        int(col) = current_coordinates[1]
+        
+        # Check above row
+        if self.verify_board_row(row-1):
+            if self.verify_board_col(col-1) and self.verify_board_piece(board[row-1][col-1].piece):
+                possible_moves.append(str(row-1) + ':' + str(col-1))
 
+            if self.verify_board_piece(board[row-1][col].piece):
+                possible_moves.append(str(row-1) + ':' + str(col))
 
-    def check_available_moves(self, board):
-        pass
+            if self.verify_board_col(col+1) and self.verify_board_piece(board[row-1][col+1].piece):
+                possible_moves.append(str(row-1) + ':' + str(col+1))
+        
+        # Check below row
+        if self.verify_board_row(row+1):
+            if self.verify_board_col(col-1) and self.verify_board_piece(board[row+1][col-1].piece):
+                possible_moves.append(str(row+1) + ':' + str(col-1))
+
+            if self.verify_board_piece(board[row+1][col].piece):
+                possible_moves.append(str(row+1) + ':' + str(col))
+
+            if self.verify_board_col(col+1) and self.verify_board_piece(board[row+1][col+1].piece):
+                possible_moves.append(str(row+1) + ':' + str(col+1))
+
+        # Check current row position
+        if self.verify_board_col(col-1) and self.verify_board_piece(board[row][col-1].piece):
+            possible_moves.append(str(row) + ':' + str(col-1))
+        
+        if self.verify_board_col(col+1) and self.verify_board_piece(board[row][col+1].piece):
+            possible_moves.append(str(row) + ':' + str(col+1))
+
+        return possible_moves
 
 
     def __str__(self):
