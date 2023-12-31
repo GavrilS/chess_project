6d296a91a87316a050bd127110f1919c36bca23f
GavrilS <gavr1lll@abv.bg>
2023-12-31 10:16:58 +0200

    Added move functionality for the queen piece

--- models_package/models/chess_pieces/queen.py
+++ models_package/models/chess_pieces/queen.py
@@ -7,12 +7,133 @@ class Queen(ChessPiece):
         super().__init__(rank='queen', color=color)
 
 
-    def move(self, board, position):
-        pass
+    def move(self, board, position, next_position):
+        possible_moves = self.check_available_moves(board, position)
+        if next_position in possible_moves:
+            return True
+        else:
+            return False
 
 
-    def check_available_moves(self, board):
-        pass
+    def check_available_moves(self, board, position):
+        possible_moves = []
+        current_coordinates = position.split(':')
+        int(row) = current_coordinates[0]
+        int(col) = current_coordinates[1]
+        current_row = row
+        current_col = col
+        flag = True
+        # Move up left diagonal
+        while flag:
+            flag = False
+            # if current_row-1 >= START_ROW and current_row-1 <= END_ROW and current_col-1 >= START_COL and current_col-1 <= END_COL:
+            if self.verify_board_row(row=current_row-1) and self.verify_board_col(col=current_col-1):
+                # if not board[current_row-1][current_col-1].piece or board[current_row-1][current_col-1].piece.color != self.color:
+                if self.verify_board_piece(board[current_row-1][current_col-1].piece):
+                    possible_moves.append(str(current_row-1) + ':' + str(current_col-1))
+                    current_row -= 1
+                    current_col -= 1
+                    flag = True
+        
+        current_row = row
+        current_col = col
+        flag = True
+        # Move up right diagonal
+        while flag:
+            flag = False
+            # if current_row-1 >= START_ROW and current_row-1 <= END_ROW and current_col+1 >= START_COL and current_col+1 <= END_COL:
+            if self.verify_board_row(row=current_row-1) and self.verify_board_col(col=current_col+1):
+                # if not board[current_row-1][current_col+1].piece or board[current_row-1][current_col+1].piece.color != self.color:
+                if self.verify_board_piece(board[current_row-1][current_col+1].piece):
+                    possible_moves.append(str(current_row-1) + ':' + str(current_col+1))
+                    current_row -= 1
+                    current_col += 1
+                    flag = True
+
+        current_row = row
+        current_col = col
+        flag = True
+        # Move down left diagonal
+        while flag:
+            flag = False
+            # if current_row+1 >= START_ROW and current_row+1 <= END_ROW and current_col-1 >= START_COL and current_col-1 <= END_COL:
+            if self.verify_board_row(current_row+1) and self.verify_board_col(current_col-1):
+                # if not board[current_row+1][current_col-1].piece or board[current_row+1][current_col-1].piece.color != self.color:
+                if self.verify_board_piece(board[current_row+1][current_col-1].piece):
+                    possible_moves.append(str(current_row+1) + ':' + str(current_col-1))
+                    current_row += 1
+                    current_col -= 1
+                    flag = True
+
+        current_row = row
+        current_col = col
+        flag = True
+        # Move down right diagonal
+        while flag:
+            flag = False
+            # if current_row+1 >= START_ROW and current_row+1 <= END_ROW and current_col+1 >= START_COL and current_col-1 <= END_COL:
+            if self.verify_board_row(current_row+1) and self.verify_board_col(current_col+1):
+                # if not board[current_row+1][current_col+1].piece or board[current_row+1][current_col+1].piece.color != self.color:
+                if self.verify_board_piece(board[current_row+1][current_col+1].piece):
+                    possible_moves.append(str(current_row+1) + ':' + str(current_col+1))
+                    current_row += 1
+                    current_col += 1
+                    flag = True
+
+        current_row = row
+        current_col = col
+        flag = True
+        # Move up the field from current position
+        while flag:
+            flag = False
+            # if current_row-1 >= START_ROW and current_row-1 <= END_ROW:
+            if self.verify_board_row(row=current_row-1):
+                # if not board[current_row-1][current_col].piece or board[current_row-1][current_col].piece.color != self.color:
+                if self.verify_board_piece(piece=board[current_row-1][current_col].piece):
+                    possible_moves.append(str(current_row-1) + ':' + str(current_col))
+                    current_row -= 1
+                    flag = True
+
+        current_row = row
+        flag = True
+        # Move down the field from current position
+        while flag:
+            flag = False
+            # if current_row+1 >= START_ROW and current_row+1 <= END_ROW:
+            if self.verify_board_row(row=current_row+1):
+                # if not board[current_row+1][current_col].piece or board[current_row+1][current_col].piece.color != self.color:
+                if self.verify_board_piece(piece=board[current_row+1][current_col].piece):
+                    possible_moves.append(str(current_row+1) + ':' + str(current_col))
+                    current_row += 1
+                    flag = True
+        
+        current_row = row
+        flag = True
+        # Move to the left from current position
+        while flag:
+            flag = False
+            # if current_col-1 >= START_COL and current_col-1 <= END_COL:
+            if self.verify_board_col(col=current_col-1):
+                # if not board[current_row][current_col-1].piece or board[current_row][current_col-1].piece.color != self.color:
+                if self.verify_board_piece(piece=board[current_row][current_col-1].piece):
+                    possible_moves.append(str(current_row) + ':' + str(current_col-1))
+                    current_col -= 1
+                    flag = True
+
+        current_col = col
+        flag = True
+        # Move to the right from current position
+        while flag:
+            flag = False
+            # if current_col+1 >= START_COL and current_col+1 <= END_COL:
+            if self.verify_board_col(col=current_col+1):
+                # if not board[current_row][current_col+1].piece or board[current_row][current_col+1].piece.color != self.color:
+                if self.verify_board_piece(piece=board[current_row][current_col+1].piece):
+                    possible_moves.append(str(current_row) + ':' + str(current_col+1))
+                    current_col += 1
+                    flag = True
+
+        return possible_moves
 
 
     def __str__(self):
